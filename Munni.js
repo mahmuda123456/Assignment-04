7e4fd5cbd414f86ee319facddd855cb3dfedea4f
mahmuda123456 <babliakter2024@gmail.com>
2024-02-20 14:48:02 +0600

    first commit

---
+++ Munni.js
@@ -0,0 +1,23 @@
+var foodPrice = 60;
+var packedWell = true;
+var masalaPrice = 30;
+var ruti = 15;
+var myBudget = 16;
+
+
+if(foodPricePrice < myBudget){
+    if(packedWell == false ){
+        console.log('chiken ami khabo na');
+    }else{
+        console.log("ruti diya kola khabo");
+    }
+
+}
+else if(masalaPrice <= myBudget && ruti < myBudget){
+    console.log('vegetable  diya food khabo')
+}else if(tustBiscut <myBudget){
+    console.log('notun akti pakhi dekho')
+}
+else{
+    console.log("pkathi ti dekte onek sundor")
+}
